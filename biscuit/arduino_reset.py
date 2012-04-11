c1fd7d7c72d3735a5d30a6b24a1e1b799dd49f4a
Giles Hall <giles@polymerase.org>
2012-04-11 06:01:00 -0400

    more arduino compat and LED debug

---
+++ biscuit/arduino_reset.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+import sys, serial, time
+
+port = "/dev/ttyUSB0"
+if len(sys.argv) > 1:
+    port = sys.argv[1]
+arduino = serial.Serial(port, 57600)
+arduino.setRTS(0)
+time.sleep(.1)
+arduino.setRTS(1)
+arduino.close()
