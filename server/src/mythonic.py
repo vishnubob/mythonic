c8ddbbc349fff9d899711c0b8b0162dd9245c457
saccharomyces <danimal@bikegasm.org>
2012-06-09 18:06:07 -0400

    Tidied up

--- server/src/mythonic.py
+++ server/src/mythonic.py
@@ -1,6 +1,11 @@
+"""
+The business logic for Sonic Storyboard
+"""
+
+from math import floor
+
 from biscuit import Manager
 from pictureframe import PictureFrame
-from math import floor
 
 class SSManager(Manager):
     """
@@ -21,12 +26,13 @@ class SSManager(Manager):
         return floor(seed + offset % 255)
 
     def think(self):
-        seconds = floor(time.time())
+        """
+        Cycle through various combinations of colors
+        """
 
         for picture_frame, idx in self.picture_frames:
             offset = idx * 10
             picture_frame.set_red(self.cook_intensity(0 + offset))
             picture_frame.set_green(self.cook_intensity(85 + offset))
             picture_frame.set_blue(self.cook_intensity(170 + offset))
-            picture_frame.set_uv(self.cook_intensity(0 + offset, 20))
-        
+            picture_frame.set_uv(self.cook_intensity(0 + offset, 2.5))
