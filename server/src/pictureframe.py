8f8d5fcf330e787e506f15f12dd9426830142f6c
saccharomyces <danimal@bikegasm.org>
2012-06-10 16:45:55 -0400

    Remove use of LightsFrame (I thought I did this?), access through HardwareChain.

--- server/src/pictureframe.py
+++ server/src/pictureframe.py
@@ -25,52 +25,52 @@ class PictureFrame(object):
     MIN_UV = 0x0
     UV_IDX = 5
 
-    def __init__(self, address, lights):
-       self.lights = lights
+    def __init__(self, address, hc):
        self.address = address
+       self.hc = hc
 
     def __str__(self):
         return "PictureFrame(addr={0}, red={1}, green={2}, blue={3}, uv={4}, white={5})".format(
             self.address, self.red, self.green, self.blue, self.uv, self.white)
 
     def get_red(self):
-        return self.lights.get_light(self.RED_IDX)
+        return self.hc.get_light(self.address, self.RED_IDX)
     def set_red(self, intensity):
         if intensity > self.MAX_RED or intensity < self.MIN_RED:
             raise ValueError("red intensity " + str(intensity) + " is out of bounds")
-        self.lights.set_light(self.RED_IDX, intensity)
+        self.hc.set_light(self.address, self.RED_IDX, intensity)
     red = property(get_red, set_red)
 
     def get_green(self):
-        return self.lights.get_light(self.GREEN_IDX)
+        return self.hc.get_light(self.address, self.GREEN_IDX)
     def set_green(self, intensity):
         if intensity > self.MAX_GREEN or intensity < self.MIN_GREEN:
             raise ValueError("green intensity " + str(intensity) + " is out of bounds")
-        self.lights.set_light(self.GREEN_IDX, intensity)
+        self.hc.set_light(self.address, self.GREEN_IDX, intensity)
     green = property(get_green, set_green)
 
     def get_blue(self):
-        return self.lights.get_light(self.BLUE_IDX)
+        return self.hc.get_light(self.address, self.BLUE_IDX)
     def set_blue(self, intensity):
         if intensity > self.MAX_BLUE or intensity < self.MIN_BLUE:
             raise ValueError("blue intensity " + str(intensity) + " is out of bounds")
-        self.lights.set_light(self.BLUE_IDX, intensity)
+        self.hc.set_light(self.address, self.BLUE_IDX, intensity)
     blue = property(get_blue, set_blue)
 
     def get_white(self):
-        return self.lights.get_light(self.WHITE_IDX)
+        return self.hc.get_light(self.address, self.WHITE_IDX)
     def set_white(self, intensity):
         if intensity > self.MAX_WHITE or intensity < self.MIN_WHITE:
             raise ValueError("white intensity " + str(intensity) + " is out of bounds")
-        self.lights.set_light(self.WHITE_IDX, intensity)
+        self.hc.set_light(self.address, self.WHITE_IDX, intensity)
     white = property(get_white, set_white)
 
     def get_uv(self):
-        return self.lights.get_light(self.UV_IDX)
+        return self.hc.get_light(self.address, self.UV_IDX)
     def set_uv(self, intensity):
         if intensity > self.MAX_UV or intensity < self.MIN_UV:
             raise ValueError("uv intensity " + str(intensity) + " is out of bounds")
-        self.lights.set_light(self.UV_IDX, intensity)
+        self.hc.set_light(self.address, self.UV_IDX, intensity)
     uv = property(get_uv, set_uv)
 
     def blackout(self):
