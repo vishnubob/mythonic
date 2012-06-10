4cea006636fe787619ba7a09ae4270550d1200a1
saccharomyces <danimal@bikegasm.org>
2012-06-09 23:02:02 -0400

    A pretty neat light show

--- server/src/mythonic.py
+++ server/src/mythonic.py
@@ -2,7 +2,8 @@
 The business logic for Sonic Storyboard
 """
 
-from math import floor
+import math
+import time
 
 from biscuit import Manager
 from pictureframe import PictureFrame
@@ -13,26 +14,36 @@ class SSManager(Manager):
     """
 
     def __init__(self, hc, number_of_boxes):
+        super(SSManager, self).__init__(hc)
         self.picture_frames = []
         for idx in range(number_of_boxes):
-            self.picture_frames.append(PictureFrame(idx, hc))
+            self.picture_frames.append(PictureFrame(idx, hc.light_frames[idx]))
 
+    def calc_intensity(self, ceiling, offset=0, rate=1):
+        """
+        Return an intensity staggered by "offset", changing at "rate"
+        """
+        seed = time.time() * rate + offset
+        unbound = int(math.sin(seed) * ceiling)
+        if unbound < 0:
+            return 0
 
-    def cook_intensity(offset=0, pace=10):
-        """
-        Return an intensity staggered by "offset", changing at "pace"
-        """
-        seed = time.time() * pace
-        return floor(seed + offset % 255)
+        return unbound % ceiling
+
+    def blackout(self):
+        for pf in self.picture_frames:
+            pf.blackout()
 
     def think(self):
         """
         Cycle through various combinations of colors
         """
 
-        for picture_frame, idx in self.picture_frames:
+        for idx, pf in enumerate(self.picture_frames):
             offset = idx * 10
-            picture_frame.set_red(self.cook_intensity(0 + offset))
-            picture_frame.set_green(self.cook_intensity(85 + offset))
-            picture_frame.set_blue(self.cook_intensity(170 + offset))
-            picture_frame.set_uv(self.cook_intensity(0 + offset, 2.5))
+            pf.set_red(self.calc_intensity(pf.MAX_RED,  0 + offset))
+            pf.set_green(self.calc_intensity(pf.MAX_GREEN, 85 + offset))
+            pf.set_blue(self.calc_intensity(pf.MAX_BLUE, 170 + offset))
+            pf.set_uv(self.calc_intensity(pf.MAX_UV, 0 + offset, 0.5))
+            print pf
+        
