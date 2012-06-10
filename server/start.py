69361680d0f124cc9c0a6b13b33566d819853f54
saccharomyces <danimal@bikegasm.org>
2012-06-09 23:00:47 -0400

    Proper harness for SSManager.  Catches SIGINT and blacks out lights

--- server/start.py
+++ server/start.py
@@ -1,19 +1,35 @@
 #!/usr/bin/env python
 
-import sys
 import serial
+import signal
+import sys
 import time
 
 from mythonic import SSManager
+from biscuit import HardwareChain
+
+PFRAME_COUNT = 3
 
 def main():
     port = serial.Serial(sys.argv[1], baudrate=1000000, parity=serial.PARITY_EVEN)
-    hc = HardwareChain(port, 2, .001)
+    hc = HardwareChain(port, PFRAME_COUNT, .001)
     hc.beacon(0)
     time.sleep(.5)
     hc.beacon(1)
     time.sleep(.5)
-    man = SSManager(hc)
+    hc.beacon(2)
+    time.sleep(.5)
+
+    man = SSManager(hc, PFRAME_COUNT)
+
+    def signal_handler(signal, frame):
+        man.blackout()
+        for i in range(PFRAME_COUNT * 2):
+            man.cycle()
+        sys.exit(0)
+
+    signal.signal(signal.SIGINT, signal_handler)
+
     man.run()
 
 if __name__ == '__main__':
