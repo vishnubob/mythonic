a8b3beef93d7d61c4a07168b40c0d1bc040b7fdf
Giles Hall <giles@polymerase.org>
2012-06-28 03:29:15 -0400

    first midi tool

---
+++ music_raw/process_percussion.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+import midi
+
+mf = midi.read_midifile("Percussion_Dance_Rhythm.mid")
+drumset = set()
+
+track = mf[0]
+for event in track:
+    if event.__class__ == midi.events.NoteOnEvent:
+        drumset.add(event.data[0])
+
+print drumset
+drumhash = {}
+for note in drumset:
+    drumhash[note] = midi.Pattern(resolution=mf.resolution)
+    _track = midi.Track()
+    drumhash[note].append(_track)
+
+for event in track:
+    if event.__class__ in (midi.events.NoteOnEvent, midi.events.NoteOffEvent):
+        drumhash[event.data[0]][0].append(event)
+    else:
+        for key in drumhash:
+            drumhash[key][0].append(event)
+
+for key in drumhash:
+    fn = "drum_%s.mid" % key
+    midi.write_midifile(fn, drumhash[key])
+
