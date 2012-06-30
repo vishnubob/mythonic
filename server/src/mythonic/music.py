1bcf260ff9485dce51cd6aee8ae8bfbb5b60b9f4
saccharomyces <danimal@bikegasm.org>
2012-06-30 14:06:43 -0400

    Progress!

--- server/src/mythonic/music.py
+++ server/src/mythonic/music.py
@@ -1,3 +1,4 @@
+import pprint
 import select
 import sys
 import time
@@ -5,8 +6,24 @@ import time
 import midi
 import midi.sequencer
 
+
 BEATS_PER_MEASURE = 4
 TEMPO = 120
+
+TEST_PATTERN = midi.Pattern(format=1, resolution=15360, tracks=\
+    [midi.Track([midi.NoteOnEvent(tick=0, channel=0, data=[72, 100]),
+      midi.NoteOffEvent(tick=15360, channel=0, data=[72, 0]),
+      midi.NoteOnEvent(tick=46080, channel=0, data=[72, 100]),
+      midi.NoteOffEvent(tick=15360, channel=0, data=[72, 0]),
+      midi.NoteOnEvent(tick=46080, channel=0, data=[72, 100]),
+      midi.NoteOffEvent(tick=15360, channel=0, data=[72, 0]),
+      midi.NoteOnEvent(tick=46080, channel=0, data=[72, 100]),
+      midi.NoteOffEvent(tick=15360, channel=0, data=[72, 0]),
+      midi.NoteOnEvent(tick=30720, channel=0, data=[65, 100]),
+      midi.NoteOffEvent(tick=15360, channel=0, data=[65, 0]),
+      midi.EndOfTrackEvent(tick=0, data=[])])])
+
+PP = pprint.PrettyPrinter(indent=4, depth=4)
 
 def main():
     if len(sys.argv) < 4:
@@ -44,7 +61,6 @@ def make_looper(midi_files, client, port, beats=BEATS_PER_MEASURE, tempo=TEMPO):
     res = patterns[0].resolution
 
     midi_writer = make_midi_writer(client, port, res)
-    setattr(midi_writer, 'started_at',  time.time())
 
     tracks = []
     for pattern in patterns:
@@ -98,13 +114,18 @@ class Looper(object):
         self.resolution = resolution
         self.tempo = tempo
 
-        self.tick_cursors = [0 for track in tracks]
-
         self.ticks_per_measure = self.beats_per_measure * self.resolution
         self.measure_s = self.beats_per_measure/(self.tempo/60.0)
-        print self.measure_s
 
-        self.offsets = [self.ticks_per_measure * 0 for track in tracks]
+        self.offsets = [0 for track in tracks]
+        self.tick_cursors = [0 for track in tracks]
+
+        # Validate that ticks are absolute
+        for track in tracks:
+            last_tick = 0
+            for event in track.events:
+                assert(event.tick >= last_tick)
+                last_tick = event.tick
 
     def play(self, idx):
         track = self.tracks[idx]
@@ -147,27 +168,36 @@ class Looper(object):
         Only events part of "now_playing" tracks are included.
         """
         upcoming_events = []
+        #TODO ticks_transpired = self.ticks_transpired
+        ticks_transpired = self.ticks_per_measure
         for idx, track in enumerate(self.tracks):
             if track not in self.now_playing:
-                pass #continue
+                self.offsets[idx] += ticks_transpired
+                continue
             lower = self.tick_cursors[idx]
-            # Off by one error somewhere here?
             upper = min(track.max_tick, lower + self.ticks_per_measure - 1)
+            # TODO min(track.max_tick, ticks_transpired - 1) 
             qualifies = lambda e: e.tick >= lower and e.tick <= upper
+            offset_events = []
+            print filter(qualifies, track.events)
             for event in filter(qualifies, track.events):
                 # Clone the event and increment its ticks
                 event = eval(repr(event))
                 event.tick = event.tick + self.offsets[idx]
-                upcoming_events.append(event)
-        print "Offsets:", self.offsets, "cursors:", self.tick_cursors
-        self._increment_measures()
+                offset_events.append(event)
+            upcoming_events += offset_events
+            self._advance_cursor(idx, ticks_transpired)
         return upcoming_events
 
-    def _increment_measures(self):
-        for idx, track in enumerate(self.tracks):
-            if self.tick_cursors[idx] >= track.max_tick:
-                self.tick_cursors[idx] = 0
-                self.offsets[idx] += track.max_tick + 1
+    @property
+    def ticks_transpired(self):
+        return int((time.time() - self.last_write) * self.ticks_per_s)
 
+    def _advance_cursor(self, idx, amount):
+        track = self.tracks[idx]
+        self.tick_cursors[idx] += amount
+        if self.tick_cursors[idx] >= track.max_tick:
+            self.tick_cursors[idx] = 0
+            self.offsets[idx] += track.max_tick + 1
 if __name__ == "__main__":
     main()
