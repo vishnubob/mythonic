5ff0489b320a8c95fdabbc6a77a05aefc9afe20f
saccharomyces <danimal@bikegasm.org>
2012-06-30 22:20:26 -0400

    Buffer by half a measure

--- server/src/mythonic/music.py
+++ server/src/mythonic/music.py
@@ -36,7 +36,8 @@ class LoopedTrack(object):
 
 class Looper(object):
     now_playing = set()
-    last_write = None
+    last_push = None
+    sequencer_started_at = None
 
     def __init__(self, tracks, midi_writer, beats_per_measure, resolution, tempo):
         self.midi_writer = midi_writer
@@ -48,15 +49,16 @@ class Looper(object):
         self.ticks_per_measure = self.beats_per_measure * self.resolution
         self.measure_s = self.beats_per_measure/(self.tempo/60.0)
 
-        self.offsets = [0 for track in tracks]
+        # To buffer, we don't start play
+        self.offsets = [self.ticks_per_measure for track in tracks]
         self.tick_cursors = [0 for track in tracks]
 
-        # Validate that ticks are absolute
-        for track in tracks:
-            last_tick = 0
-            for event in track.events:
-                assert(event.tick >= last_tick)
-                last_tick = event.tick
+        self.restart_sequencer()
+
+    def restart_sequencer(self):
+        self.midi_writer.stop_sequencer()
+        self.midi_writer.start_sequencer()
+        self.sequencer_started_at = time.time()
 
     def play(self, idx):
         track = self.tracks[idx]
@@ -71,20 +73,17 @@ class Looper(object):
     @property
     def need_push(self):
         now = time.time()
-        return self.last_write is None or now - self.last_write >= self.measure_s
+        if self.last_push is None:
+            # We want to keep ahead by half a measure
+            return now - self.sequencer_started_at >= self.measure_s/2.0
+        return self.last_push is None or now - self.last_push >= self.measure_s
 
     def think(self):
         if not self.need_push:
             return
-        now = time.time()
-        if self.last_write is None:
-            # Reset sequencer to reset tick count
-            self.midi_writer.stop_sequencer()
         for event in self.next_measure():
             self.write_event(event)
-        if self.last_write is None:
-            self.midi_writer.start_sequencer()
-        self.last_write = time.time()
+        self.last_push = time.time()
 
     def write_event(self, event):
         #print "write_event(", event, ")"
@@ -99,15 +98,12 @@ class Looper(object):
         Only events part of "now_playing" tracks are included.
         """
         upcoming_events = []
-        #TODO ticks_transpired = self.ticks_transpired
-        ticks_transpired = self.ticks_per_measure
         for idx, track in enumerate(self.tracks):
             if track not in self.now_playing:
-                self.offsets[idx] += ticks_transpired
+                self.offsets[idx] += self.ticks_per_measure
                 continue
             lower = self.tick_cursors[idx]
             upper = min(track.max_tick, lower + self.ticks_per_measure - 1)
-            # TODO min(track.max_tick, ticks_transpired - 1) 
             qualifies = lambda e: e.tick >= lower and e.tick <= upper
             offset_events = []
             for event in filter(qualifies, track.events):
@@ -116,12 +112,8 @@ class Looper(object):
                 event.tick = event.tick + self.offsets[idx]
                 offset_events.append(event)
             upcoming_events += offset_events
-            self._advance_cursor(idx, ticks_transpired)
+            self._advance_cursor(idx, self.ticks_per_measure)
         return upcoming_events
-
-    @property
-    def ticks_transpired(self):
-        return int((time.time() - self.last_write) * self.ticks_per_s)
 
     def _advance_cursor(self, idx, amount):
         track = self.tracks[idx]
