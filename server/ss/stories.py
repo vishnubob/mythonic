b7cdf09776d969ad9d82a44e8e8260e3dd3e654d
saccharomyces <danimal@bikegasm.org>
2012-07-27 23:53:35 -0400

    Moved non essential ss/stories.py code to unused.py along with fiddling to make the code more adjustable navigatable

--- server/ss/stories.py
+++ server/ss/stories.py
@@ -9,15 +9,13 @@ class SSManager(manager.StoryManager):
     """
     Manages which stories get activated next
     """
-    SCREENSAVER_TIMEOUT = 60 * 3
+    SCREENSAVER_TIMEOUT = 3#60 * 3
+    SCREENSAVER_SPAN = 5
+
 
     def __init__(self, hc, storyboard, looper=None):
-        self.screensaver = Screensaver(storyboard, span=10)
+        self.screensaver = Screensaver(storyboard, span=self.SCREENSAVER_SPAN)
         self.instrument = Instrument(storyboard, looper)
-        self.startup_test = StartupTest(storyboard)
-        self.dice = Dice(storyboard)
-        self.blackout_game = BlackoutGame(storyboard)
-        self.chaos = Chaos(storyboard)
         time_per_frame = 5
         self.naratives = [
             BatAdventure(storyboard, looper, time_per_frame),
@@ -32,17 +30,14 @@ class SSManager(manager.StoryManager):
     def select_story(self):
         current = self.current_story
         if current is None:
-            #return self.naratives[0]
-            #return self.screensaver
-            #return self.startup_test
             return self.instrument
-            #return self.chaos
         if isinstance(current, Instrument):
             untouched_since = max(current.stage_started_at, self.storyboard.untouched_since)
             if time.time() - untouched_since >= self.SCREENSAVER_TIMEOUT:
                return self.screensaver
             if self.storyboard.pattern_complete:
                return self.storyboard.target_pattern.triggered_story
+            return self.instrument
         if isinstance(current, Screensaver):
             if self.storyboard.touched:
                 return self.instrument
@@ -53,90 +48,6 @@ class SSManager(manager.StoryManager):
             return current
         # Default to Instrument
         return self.instrument
-
-class BlackoutGame(manager.Story):
-
-    def __init__(self, storyboard):
-        super(BlackoutGame, self).__init__(storyboard)
-        self.blacked_out = [random.choice(self.storyboard)]
-
-    @property
-    def lighted(self):
-        return [pf for pf in self.storyboard if pf not in self.blacked_out]
-
-    def plot(self, t):
-        for pf in self.storyboard:
-            if pf.touched:
-                if pf in self.blacked_out:
-                    self.blacked_out.remove(pf)
-                    if len(self.lighted) > 0:
-                        candidates = self.lighted
-                        candidates.remove(pf)
-                    if len(self.lighted) > 0:
-                        self.blacked_out.append(random.choice(candidates))
-                else:
-                    self.blacked_out.append(pf)
-            if pf in self.blacked_out:
-                pf.blackout()
-            else:
-                pf.white = pf.MAX_WHITE
-        return len(self.lighted) > 0
-
-class Chaos(manager.Story):
-    def plot(self, t):
-        for pf in self.storyboard:
-            pf.hsv = tuple(random.random() for i in range(3))
-            pf.rgb = tuple(random.random() * 255 for i in range(3))
-            pf.white = random.random() * pf.MAX_WHITE
-            pf.uv = random.random() * pf.MAX_UV
-        return True
-
-class Dice(manager.Story):
-
-    def randomize(self, pf, saturation=1):
-        pf.hsv = (random.random(), saturation, 1)
-        pf.uv = pf.MAX_UV if random.random() >= 0.5 else pf.MIN_UV
-
-    def setup(self, t):
-        for pf in self.storyboard:
-            self.randomize(pf)
-
-    def plot(self, t):
-        for idx, pf in enumerate(self.storyboard):
-            if idx > t:
-                self.randomize(pf)
-        return t < len(self.storyboard) + 10
-
-class MuffinTest(manager.Story):
-
-    def plot(self, t):
-        color = int(mmath.travel(t, 20, 0, 5))
-        for pf in self.storyboard:
-            if pf.touched:
-                print "[TOUCH] Human address %d" % (pf.human_address)
-            pf.blackout()
-            if color == 0:
-               pf.red = pf.MAX_RED
-            elif color == 1:
-               pf.green = pf.MAX_GREEN
-            elif color == 2:
-               pf.blue= pf.MAX_BLUE
-            elif color == 3:
-               pf.white = pf.MAX_WHITE
-            elif color >= 4:
-               pf.uv = pf.MAX_UV
-        return color < 5
-
-class StartupTest(manager.Story):
-
-    def plot(self, t):
-        for idx, pf in enumerate(self.storyboard):
-            pf.blackout()
-            if pf.real_address < t:
-                pf.blue = pf.MAX_BLUE
-            else:
-                pf.red = pf.MAX_RED
-        return not self.storyboard.touched
 
 class Instrument(manager.MusicalStory):
 
@@ -161,7 +72,7 @@ class Instrument(manager.MusicalStory):
                 pf.uv = int(mmath.sin_abs(t / 3, True) * pf.MAX_UV)
                 if self.storyboard.in_target_pattern(pf):
                     pf.pattern_hint(t)
-            elif not self.storyboard.activated:
+            elif not self.storyboard.active_frames:
                 pf.white = pf.MAX_WHITE / 3
         return True
 
@@ -191,9 +102,6 @@ class Narative(manager.MusicalStory):
         self.last_focus = None
         super(Narative, self).__init__(storyboard, looper)
 
-    def play_music(self, focus):
-        print focus.__class__.__name__
-
     def plot(self, t):
         if len(self.foci) == 0:
             return False
@@ -204,7 +112,6 @@ class Narative(manager.MusicalStory):
         focus = self.foci[focus_idx]
         if self.last_focus != focus:
             self.last_focus = focus
-            self.play_music(focus)
         for pf in self.storyboard:
             if pf == focus:
                 continue
